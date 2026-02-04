ae78f301b2729c37cfe6b4a7367bf92feb4627f7
aj-codes-hub <aalijjaan@gmail.com>
2026-02-04 20:13:49 +0500

    herosection slider

--- src/Pages/Home/Components/HeroSection/Card1.tsx
+++ src/Pages/Home/Components/HeroSection/Card1.tsx
@@ -20,14 +20,12 @@ const handleNavigateTo = () =>{
         <>     
           
            <div 
-                className="w-[55%] h-[88%] flex mx-auto my-auto font-semibold
-                           flex-col pl-[30px] pr-[73px]">
+                className="lg:w-[55%]  h-[88%] flex lg:mx-auto my-auto font-semibold
+                           flex-col pl-[30px] 2xl:pr-[73px] xl:pr-[53px] lg:pr-[43px] pr-[33px]">
                                   
-                  <h1 className="text-[15px]
-                                 sm:text-[19px]
-                                 md:text-[20px]
-                                 lg:text-[22px]
-                                 xl:text-[24px]
+                  <h1 className="text-[30px]
+                                 sm:text-[30px]
+                                 md:text-[30px]
                                  2xl:text-[36px] 
                   ">
                     {ComicData.title}
@@ -35,7 +33,7 @@ const handleNavigateTo = () =>{
                     
                   
                     
-                        <div className="gap-[3px] flex items-center mt-[16px]
+                        <div className="gap-[3px] flex items-center lg:mt-[16px] mt-[10px]
                                         md:gap-[4px]
                                         lg:gap-[5px]
                                         xl:gap-[6px]
@@ -47,7 +45,7 @@ const handleNavigateTo = () =>{
                              className='w-[12px]         h-[12px]
                                         md:w-[12px]      md:h-[12px]
                                         lg:w-[13px]      lg:h-[13px]
-                                        xl:w-[18px]      xl:h-[18px]
+                                        xl:w-[20px]      xl:h-[20px]
                                         2xl:w-[20px]     2xl:h-[20px]
 
                              '/> 
@@ -56,7 +54,7 @@ const handleNavigateTo = () =>{
                        <span className="text-[8px]      ml-[7px]
                                         md:text-[10px]   md:ml-[8px]
                                         lg:text-[16px]   lg:ml-[6px]
-                                        xl:text-[15px]   xl:ml-[11px]
+                                        xl:text-[16px]   xl:ml-[11px]
                                         2xl:text-[18px]  2xl:ml-[11px]
                        ">
                         Action
@@ -65,20 +63,20 @@ const handleNavigateTo = () =>{
                   
                   
 
-                  <div className="w-[60%] sm:w-[95%] opacity-[60%] mt-[26px]">
-                    <p className="text-[8px]       leading-[12px]  
-                                 sm:text-[10px]   sm:leading-[14px]  
-                                 md:text-[11px]   md:leading-[14px]
-                                  lg:text-[12px]   lg:leading-[18px]
-                                  xl:text-[16px]   xl:leading-[24px]
+                  <div className="w-full opacity-[60%] lg:mt-[26px] mt-[13px] ">
+                    <p className="text-[18px]       leading-[24px]  
+                                 sm:text-[18px]   sm:leading-[24px]  
+                                 md:text-[18px]   md:leading-[24px]
+                                  lg:text-[18px]   lg:leading-[23px]
+                                  xl:text-[18px]   xl:leading-[26px]
                                   2xl:text-[20px]  2xl:leading-[34px]
                     ">
                        {ComicData.description}
                     </p>
                   </div>
 
-                  <h1 className="text-[20px] font-semibold mt-[24px]
-                                 md:text-[16px]
+                  <h1 className="text-[20px] font-semibold lg:mt-[24px] mt-[18px]
+                                 md:text-[20px]
                                  lg:text-[20px]
                                  xl:text-[26px]
                                  2xl:text-[28px]
@@ -86,8 +84,8 @@ const handleNavigateTo = () =>{
                     Author 
                   </h1>
   
-                  <div className="h-[38px]     w-[38px]  mt-[22px] rounded-full overflow-hidden
-                                  md:h-[42px] md:w-[42px]
+                  <div className="h-[38px]     w-[38px]  lg:mt-[22px]  mt-[18px] rounded-full overflow-hidden
+                                  md:h-[52px] md:w-[52px]
                                   lg:h-[46px] lg:w-[46px] 
                                   xl:h-[50px] xl:w-[50px] 
                                   2xl:h-[54px] 2xl:w-[54px] 
@@ -96,7 +94,7 @@ const handleNavigateTo = () =>{
                      <img src={ComicData.authorImage} className='scale-125'/>
                   </div>
 
-                   <div className="gap-[6px] flex mt-[45px]
+                   <div className="gap-[6px] flex 2xl:mt-[45px] xl:mt-[30px]  mt-[25px] 
                                    md:gap-[14px]  
                                    lg:gap-[15px] 
                                    xl:gap-[20px] 
--- src/Pages/Home/Components/HeroSection/HeroSection2.tsx
+++ src/Pages/Home/Components/HeroSection/HeroSection2.tsx
@@ -11,9 +11,9 @@ import CircleShadow from '../../../../Components/layout/CircleShadow';
 
 
 const cardAndData = [
-  { id: 3, content: <Card3 />, AnimeImg: "/Images/AnimeCard3.jpg"},  
+  { id: 3, content: <Card1 />, AnimeImg: "/Images/AnimeCard3.jpg"},  
   { id: 1, content: <Card1 />, AnimeImg: "/Images/AnimeCard2.png"},
-  { id: 2, content: <Card2 />, AnimeImg: "/Images/AnimeCard1.png"},
+  { id: 2, content: <Card1 />, AnimeImg: "/Images/AnimeCard1.png"},
 ];
 
 
@@ -46,11 +46,11 @@ const HeroSection2:React.FC = () => {
 
 
    return (
-     <div className='mt-[100px] relative'>
+     <div className='mt-[100px] relative flex flex-col items-center'>
 
         <CircleShadow blurClass='blur-[150px]' className="top-1/2 -translate-y-1/2 left-65 z-[200] bg-[radial-gradient(circle_at_center,#159777,#7BA49A)]"/>
 
-        <div className="w-full mx-auto">
+        <div className="2xl:w-[1920px] xl:w-[1750px] lg:w-[1500px] w-[95%] mx-auto lg:mx-0">
         <Swiper
             modules={[Navigation]}
             slidesPerView={1.7250}
@@ -63,17 +63,41 @@ const HeroSection2:React.FC = () => {
         {cardAndData.map((item)=>(
 
            <SwiperSlide key={item.id}>
-                <div className='h-[750px] w-[1200px] flex justify-center items-end'>
-                    <div className='flex h-[670px] w-[1170px] rounded-[20px] bg-[#FFFFFF0D] card duration-500 ease-out'>
+                <div className='flex justify-center items-end mx-auto lg:mx-0
+                                h-[850px] w-[310px] 
+                                md:h-[850px] md:w-[600px] 
+                                lg:h-[650px] lg:w-[920px] 
+                                xl:h-[700px] xl:w-[1120px] 
+                                2xl:h-[750px] 2xl:w-[1200px]  
+                '>
+                   
+                   
+                    <div className='rounded-[20px] bg-[#FFFFFF0D] card duration-500 ease-out flex flex-col lg:flex-row 
+                                    w-full      h-[600px] 
+                                    md:w-[450px]  md:h-[600px]
+                                    lg:w-[900px]  lg:h-[560px]
+                                    xl:w-[1040px]  xl:h-[560px]
+                                    2xl:w-[1170px] 2xl:h-[670px]
+                                    '>
                         
-                        <div className='w-[512px] h-full relative'>
-                            <div className='w-[482px] h-[685px] rounded-[20px] absolute left-[30px] top-[-64px] overflow-hidden cardImg'>
+                        <div className='lg:h-full md:h-[300px] h-[300px] relative
+                                       lg:w-[438px]
+                                       xl:w-[438px]
+                                       2xl:w-[512px]
+                        '>
+                            <div className='rounded-[20px] absolute left-[30px] lg:bottom-[64px] bottom-0 overflow-hidden cardImg
+                                            w-[222px]     h-[320px]
+                                            md:w-[222px]  md:h-[320px]
+                                            lg:w-[382px]  lg:h-[585px]
+                                            xl:w-[402px]  xl:h-[585px]
+                                            2xl:w-[482px] 2xl:h-[685px]
+                                            '>
 
                                 <div className={`bg-[#33d6ff46] play-Button rounded-full z-30 absolute items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
                                             h-[85px] w-[85px] 
                                             md:h-[95px] md:w-[95px] 
                                             lg:h-[105px] lg:w-[105px]
-                                            xl:h-[115px] xl:w-[115px]`}>s
+                                            xl:h-[115px] xl:w-[115px]`}>
 
                                    <div className="h-[70%] w-[70%] bg-[#33D6FF] rounded-full flex items-center justify-center">
                                        <img src="/Icons/PlayVideo.png"/>
@@ -85,7 +109,7 @@ const HeroSection2:React.FC = () => {
                             </div>
                         </div>
                        
-                      
+                         
                            {item.content}
 
                         
